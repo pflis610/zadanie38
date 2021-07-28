141e5588f9284ccc5a9c55378442fea7cb510bea
pflis <piotrek610@wp.pl>
2021-07-28 17:14:57 +0200

    zadanie38

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let liczba1 = 1;
+let liczba2 = 2;
+if (liczba1+liczba2 >= 0) {
+    console.log('Wynik dodawania wynosi '+ (liczba1+liczba2));
+} else {
+    console.log('Wynik jest nieprawidłowy')
+}
+if (liczba1-liczba2 >= 0) {
+    console.log('Wynik odejmowania wynosi ' + (liczba1-liczba2));
+} else {
+    console.log('Wynik jest nieprawidłowy');
+}
+if (liczba1*liczba2 >= 0) {
+    console.log('Wynik mnożenia wynosi ' + (liczba1*liczba2));
+} else {
+    console.log('Wynik jest nieprawidłowy');
+}
