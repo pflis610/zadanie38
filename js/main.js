9c62fd9e33ab9ae5746980b591df413e21cab50b
pflis610 <piotrek610@wp.pl>
2021-07-30 21:09:51 +0200

    zadanie38 korekta

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
 let liczba1 = 1;
 let liczba2 = 2;
+let numbercheck1 = function (liczba1, liczba2) {
 if (liczba1+liczba2 >= 0) {
-    console.log('Wynik dodawania wynosi '+ (liczba1+liczba2));
+    return 'Wynik dodawania wynosi '+ (liczba1+liczba2);
 } else {
-    console.log('Wynik jest nieprawidłowy')
+    return 'Wynik jest nieprawidłowy';
+    }
 }
+console.log(numbercheck1(liczba1, liczba2));
+let numbercheck2 = function (liczba1, liczba2) {
 if (liczba1-liczba2 >= 0) {
-    console.log('Wynik odejmowania wynosi ' + (liczba1-liczba2));
+    return 'Wynik odejmowania wynosi ' + (liczba1-liczba2);
 } else {
-    console.log('Wynik jest nieprawidłowy');
+    return 'Wynik jest nieprawidłowy';
+    }
 }
+console.log(numbercheck2(liczba1, liczba2));
+let numbercheck3 = function (liczba1, liczba2) {
 if (liczba1*liczba2 >= 0) {
-    console.log('Wynik mnożenia wynosi ' + (liczba1*liczba2));
+    return 'Wynik mnożenia wynosi ' + (liczba1*liczba2);
 } else {
-    console.log('Wynik jest nieprawidłowy');
+    return 'Wynik jest nieprawidłowy';
+    }
 }
+console.log(numbercheck3(liczba1, liczba2));
