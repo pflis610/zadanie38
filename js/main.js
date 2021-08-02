01c1eb8cd4842f9c895a62b8a37cdbf1105e44ae
pflis610 <piotrek610@wp.pl>
2021-08-02 14:55:56 +0200

    zadanie38 dopieszczenie

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 let liczba1 = 1;
 let liczba2 = 2;
-let numbercheck1 = function (liczba1, liczba2) {
-if (liczba1+liczba2 >= 0) {
-    return 'Wynik dodawania wynosi '+ (liczba1+liczba2);
-} else {
-    return 'Wynik jest nieprawidłowy';
-    }
+let numbercheck1 = function (licz1, licz2) {
+    if (licz1+licz2 >= 0) {
+    return 'Wynik dodawania wynosi '+ (licz1+licz2);
+    } else {
+      return 'Wynik jest nieprawidłowy';
+      }
 }
 console.log(numbercheck1(liczba1, liczba2));
-let numbercheck2 = function (liczba1, liczba2) {
-if (liczba1-liczba2 >= 0) {
-    return 'Wynik odejmowania wynosi ' + (liczba1-liczba2);
-} else {
-    return 'Wynik jest nieprawidłowy';
-    }
+let numbercheck2 = function (licz1, licz2) {
+    if (licz1-licz2 >= 0) {
+    return 'Wynik odejmowania wynosi ' + (licz1-licz2);
+    } else {
+      return 'Wynik jest nieprawidłowy';
+      }
 }
 console.log(numbercheck2(liczba1, liczba2));
-let numbercheck3 = function (liczba1, liczba2) {
-if (liczba1*liczba2 >= 0) {
-    return 'Wynik mnożenia wynosi ' + (liczba1*liczba2);
-} else {
-    return 'Wynik jest nieprawidłowy';
-    }
+let numbercheck3 = function (licz1, licz2) {
+    if (licz1*licz2 >= 0) {
+    return 'Wynik mnożenia wynosi ' + (licz1*licz2);
+    } else {
+      return 'Wynik jest nieprawidłowy';
+      }
 }
 console.log(numbercheck3(liczba1, liczba2));
